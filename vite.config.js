bdea99301f5dc6488034ca3a8339c71ea3d8a449
vokir <volkodav.lol31@gmail.com>
2022-07-22 19:08:04 +0500

    cors

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()]
+  server: {
+    cors: false,
+    proxy: {
+      "/api": {
+        target: 'https://bitrix-stage.culture-home.ru/',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      }
+    }
+  },
+  plugins: [vue()],
 })
