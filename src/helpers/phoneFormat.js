43a201b377a43ed93e841bf69118c5164f428420
provirtys <provirtys@gmail.com>
2022-12-06 17:10:00 +0500

    phone number fix

--- src/helpers/phoneFormat.js
+++ src/helpers/phoneFormat.js
@@ -4,7 +4,7 @@ const computePhone = (s) => {
   if (phone.length === 11) {
     phone = phone.substr(1);
   }
-  return phone.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/g, `+7 $1 $2-$3-$4`);
+  return phone.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/g, `+7 ($1) $2-$3-$4`);
 };
 
 export default computePhone
